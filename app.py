d80bdefccdaad02973dbaa42226b64cac9e017c7
山口 遥己 <77283970+8maguchi8ruki@users.noreply.github.com>
2023-12-06 10:26:58 +0900

    style変更後のapp.py

--- app.py
+++ app.py
@@ -4,29 +4,53 @@ from tkinter import filedialog
 from PIL import ImageTk, Image
 from ultralytics import YOLO
 import os, time, datetime, threading, subprocess, cv2, shutil
+import customtkinter
 
-class Application(tk.Frame):
-    ########## 初期設定 ##########
-    def __init__(self,master):
-        super().__init__(master)
-        self.pack()
-        self.master.geometry("1000x650")
-        self.master.resizable(width="False",height="False")
-        self.master.title("YOLOplus")
+FONT_TYPE = "meiryo"
 
-    ########## 画像の縦横比率を維持してリサイズする関数 ##########
-    def resizing(img,width):
+class App(customtkinter.CTk):
+
+    def __init__(self):
+        super().__init__()
+        self.fonts = (FONT_TYPE, 15)
+        self.geometry("1000x650")
+        self.title("YOLOPlus")
+        self.resizable(width="False",height="False")
+        self.setup_form()
+
+    
+    def setup_form(self):
+        customtkinter.set_appearance_mode("dark")
+        customtkinter.set_default_color_theme("green")
+
+        frame1 = customtkinter.CTkFrame(master=self,width=500)
+        frame1.place(x=150, y=50)
+        global file_path, filepath_entry
+        file_path = customtkinter.StringVar()
+        filepath_entry = customtkinter.CTkEntry(frame1, width=500, font=self.fonts,textvariable=file_path)
+        filepath_entry.place(x=0, y=0)
+
+        self.button = customtkinter.CTkButton(master=self, text="参照", command=self.click_refer_button, font=self.fonts,width=80)
+        self.button.place(x=670, y=50)
+        self.button2 = customtkinter.CTkButton(master=self, text="表示", command=self.click_display_button, font=self.fonts,width=80)
+        self.button2.place(x=770, y=50)
+        self.yolo_button = customtkinter.CTkButton(self, text=u'画像認識', command=self.click_detect_button)
+        self.yolo_button.place(x=870,y=50)
+    
+        ########## 画像の縦横比率を維持してリサイズする関数 ##########
+    def resizing(self,img,width):
         height = round(img.height * width/ img.width)
         return img.resize((width,height))
     
-    ########## 参照の関数（参照ボタンを押すと実行される） ##########
+
+    ########## 参照の関数 ##########
     def click_refer_button(self):
         fTyp = [("JPEG",".jpg"),("PNG",".png")]
         iDir = os.path.abspath(os.path.dirname(__file__))
         filepath = filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
         file_path.set(filepath)
 
-    ########## 表示の関数（表示ボタンを押すと実行される） ##########
+    ########## 表示の関数 ##########
     def click_display_button(self):
         tval = filepath_entry.get()
         print("入力されたパス")
@@ -40,33 +64,16 @@ class Application(tk.Frame):
         # 画像保存
         img = Image.open(tval)
         global img_resize, display 
-        img_resize = Application.resizing(img,380)
+        img_resize = self.resizing(img,600)
         img_resize.save("C:/GUI/img/" + filename)
         resized_img = Image.open("C:/GUI/img/" + filename)
         # 画像表示
         display = ImageTk.PhotoImage(resized_img)
-        canvas = tk.Canvas(width=400, height=500,bg="#000", bd=5,highlightbackground="red")
-        canvas.place(x=67,y=100)
-        canvas.create_image(207.5,250,image=display)
-
-    ########## 高画質化の関数（高画質化ボタンを押すと実行される） ##########
-    def click_refine_button(self):
-        global img_resize, display
-        img_resize.save("C:/GUI/SwinIR/testsets/RealSRSet+5images/" + filename)
-        command = "python SwinIR/main_test_swinir.py --task real_sr --scale 4 --large_model --model_path model_zoo/swinir/003_realSR_BSRGAN_DFOWMFC_s64w8_SwinIR-L_x4_GAN.pth --folder_lq SwinIR/testsets/RealSRSet+5images"
-        subprocess.run(command,shell=True)
-        img = Image.open("C:/GUI/SwinIR/results/swinir_real_sr_x4_large/" + filename)
-        # 画像保存
-        img_resize = Application.resizing(img,380)
-        img_resize.save("C:/GUI/img/" + filename)
-        resized_img = Image.open("C:/GUI/SwinIR/results/swinir_real_sr_x4_large/" + filename)
-        # 画像表示
-        display = ImageTk.PhotoImage(resized_img)
-        self.canvas1 = tk.Canvas(width=400, height=500,bg="#333", relief="solid", bd=5, highlightbackground="red")
-        self.place(x=60,y=100)
-        self.canvas1.create_image(207.5,250,image=display)
-
-    ########## 画像認識の関数（画像認識ボタンを押すと実行される） ##########
+        canvas = customtkinter.CTkCanvas(width=600, height=700,bg="#333", bd=5,highlightbackground="green")
+        canvas.place(x=100,y=200)
+        canvas.create_image(305,375,image=display)
+        
+    ########## 画像認識の関数 ##########
     def click_detect_button(self):
         global img_resize
         img_resize.save("C:/GUI/yolov5/data/images/" + "yolo_" + filename)
@@ -79,7 +86,7 @@ class Application(tk.Frame):
         print(files)
         img = Image.open("C:/GUI/yolov5/runs/detect/"+ files[-1] + "/" + "yolo_" + filename)
         # 画像保存
-        self.img_resize = Application.resizing(img,380)
+        self.img_resize = self.resizing(img,380)
         self.img_resize.save("C:/GUI/img/" + "yolo_" + filename)
         self.resized_img = Image.open("C:/GUI/img/" + "yolo_" + filename)
         print(self.resized_img)
@@ -91,125 +98,12 @@ class Application(tk.Frame):
         self.canvas2.place(x=500,y=100)
         self.canvas2.create_image(207.5,250,image=display2)
 
-    ########## カメラ起動して物体検知、画像保存する関数（撮影開始ボタンを押すと実行される） ##########
-    def click_camera_button(self):
-        self.judge = 0
-        try:
-            shutil.rmtree("C:/GUI/runs/detect/")
-        except:
-            print("I can't find!!!")
-        ##### 画像が保存されたか確認、されてたらそのときの日時を取得する関数 #####
-        def check_get_time():
-            if self.judge == 1:
-                return
-            print('Connecting ...')
-            path = "C:/GUI/runs/detect/predict/crops/person/"
-            try:
-                global directory, detect_time, saved_number
-                directory = os.listdir(path)
-                length_small = len(directory)
-                detect_time = []
-                saved_number = []
-                while True:
-                    if self.judge == 1:
-                        break
-                    print("画像が保存されたかの確認処理")
-                    directory = os.listdir(path)
-                    length_large = len(directory)
-                    print("検出回数")
-                    print(length_small,length_large)
-                
-                    if length_small < length_large:
-                        print("新しい画像が保存されました")
-                        detect_time.append(str(datetime.datetime.now().replace(microsecond=0))) #日時取得
-                        num = length_large - length_small
-                        saved_number.append(num)
-                        length_small = length_large
-                        print("一度の検知で保存された画像枚数のデータ：")
-                        print(saved_number)
-                        print("検知した日時のデータ：：")
-                        print(detect_time)
-                        time.sleep(1)
-            except:
-                time.sleep(1)
-                check_get_time()
-        thread1 = threading.Thread(target=check_get_time) # 上のcheck_get_time関数は別のスレッドで実行（並列処理）
-        thread1.start()
-
-        def save_frame_img(self):
-                    Application.datas = []
-                    j = 0 # 時刻参照用
-                    k = 0 # 画像参照用
-                    print(saved_number,detect_time)
-                    shutil.rmtree("C:/GUI/result/detected_imgs/")
-                    os.mkdir("C:/GUI/result/detected_imgs")
-                    for n in saved_number:
-                        i = 0 #n回に到達するまでのカウント用
-                        while i <= n:
-                            edit = str(detect_time[j]).replace(":","-").replace(" ","_")
-                            d = edit + "_"+ directory[k]
-                            self.datas.append(d)
-                            img = Image.open("C:/GUI/runs/detect/predict/crops/person/" + directory[k])
-                            # 画像リサイズ
-                            self.img_resize = Application.resizing(img,250)
-                            self.img_resize.save("C:/GUI/result/detected_imgs/" + self.datas[k])
-                            i = i + 1
-                            k = k + 1
-                        j = j + 1
-                    print(self.datas)
-        
-        model = YOLO('yolov8n.pt')
-        cap = cv2.VideoCapture(0)
-        while cap.isOpened():
-            success, frame = cap.read()
-            if success:
-                global results
-                results = model(frame,show=True,save=True,save_crop=True)
-                if cv2.waitKey(1) & 0xFF == ord("q"):
-                    self.judge = self.judge + 1
-                    cap.release()
-                    cv2.destroyAllWindows()
-                    save_frame_img(self)
-                    break
-            else:
-                break
-        cap.release()
-        cv2.destroyAllWindows()
+    def button_function(self):
+        print(self.textbox.get())
 
 
-########## こっから下はアプリの見た目（ボタンとか）##########
-def main():
-    win = tk.Tk()
-    app = Application(master=win)
-    frame1 = ttk.Frame(app,padding=10)
-    frame1.grid()
-    # ラベル
-    s = tk.StringVar()
-    s.set("ファイル名")
-    label1 = ttk.Label(frame1,textvariable=s)
-    label1.grid(row=0,column=0)
-    # ファイル名を入力するテキストボックス
-    global file_path, filepath_entry
-    file_path = tk.StringVar()
-    filepath_entry = ttk.Entry(frame1,textvariable=file_path,width=50)
-    filepath_entry.grid(row=0,column=1)
-    
-    # 参照ボタン
-    refer_button = ttk.Button(app,text=u"参照",command=app.click_refer_button)
-    refer_button.grid(row=0, column=2)
-    # 表示ボタン
-    display_button = ttk.Button(app, text=u'表示', command=app.click_display_button)
-    display_button.grid(row=0, column=3)
-    # 高画質化ボタン
-    refine_button = ttk.Button(app, text=u'高画質化', command=app.click_refine_button)
-    refine_button.grid(row=1, column=2)
-    # 画像認識ボタン
-    yolo_button = ttk.Button(app, text=u'画像認識', command=app.click_detect_button)
-    yolo_button.grid(row=1, column=3)
-    # 撮影開始ボタン
-    yolo_button2 = ttk.Button(app, text=u'撮影開始', command=app.click_camera_button)
-    yolo_button2.grid(row=1, column=4)
-
-    app.mainloop()
 if __name__ == "__main__":
-    main()
+    app = App()
+    app.mainloop()
+
+
